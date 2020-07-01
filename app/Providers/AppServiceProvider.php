b2d1c21cb69327dea811c5d42741882ed54f111e
Christopher <christopherssalvador@gmail.com>
2020-07-01 11:21:25 +0800

    removed data wrapping for json response

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Http\Resources\Json\JsonResource;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -23,6 +23,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // remove data[] wrap for resource
+        JsonResource::withoutWrapping();
     }
 }
